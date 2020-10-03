d625568d1f2ada98857495f5e5f4ddde888413b8
NerdyPunk <terceromedia@gmail.com>
2020-10-03 14:14:53 -0600

    Pong Version 01 Original

--- pong-1/main.lua
+++ pong-1/main.lua
@@ -10,9 +10,7 @@
     Author: Colton Ogden
     cogden@cs50.harvard.edu
 
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
+    Originalmente programador por Atari en 1972
 
     This version is built to more closely resemble the NES than
     the original Pong machines or the Atari 2600 in terms of
