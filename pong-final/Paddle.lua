243eef2d299b2afb21cb87e529d21044713b541d
NerdyPunk <terceromedia@gmail.com>
2020-10-12 09:25:36 -0600

    AI Update

--- pong-final/Paddle.lua
+++ pong-final/Paddle.lua
@@ -50,6 +50,14 @@ function Paddle:update(dt)
     end
 end
 
+function Paddle:updateAI(ball)
+    if ball.dy < 0 then
+        self.y = math.max(0, ball.y + (ball.height / 2) - (self.height / 2))
+    else
+        self.y = math.min(VIRTUAL_HEIGHT - self.height, ball.y + (ball.height / 2) - (self.height /2))
+    end
+end
+
 --[[
     To be called by our main function in `love.draw`, ideally. Uses
     LÖVE2D's `rectangle` function, which takes in a draw mode as the first
